4f6f2af89c6f1690ca2f96239473026a2bfbd70d
Meghan Donaghy <meghan.donaghy@zopa.com>
2024-10-19 15:33:55 +0100

    Put tests in place for rowColChecker

--- test/sudokuClass.test.js
+++ test/sudokuClass.test.js
@@ -1,3 +1,26 @@
-const SudokuClass = require('../js/sudokuClass'); 
-const { expect } = require('chai');
+const Sudoku = require("../js/sudokuClass");
+const assert = require('assert');
 
+describe("Sudoku", () => {
+  describe(".rowColChecker", () => {
+    it("should return true when the given value exists in the given array", () => {
+      //setup
+        const array = [1,2,3,4];
+        const val = 4;
+      //exercise
+      const result = Sudoku.rowColChecker(array,val);
+      //verify
+      assert.ok(result === true);
+    });
+
+    it("should return false when the given value does not exist in the given array", () => {
+        //setup
+          const array = [1,2,3];
+          const val = 4;
+        //exercise
+        const result = Sudoku.rowColChecker(array,val);
+        //verify
+        assert.ok(result === false);
+      });
+  });
+});
