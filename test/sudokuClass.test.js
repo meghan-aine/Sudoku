3e3b21709202767b39cc20b575459d82eb0a739b
Meghan Donaghy <meghan.donaghy@zopa.com>
2024-10-19 16:43:34 +0100

    Add test on solution

--- test/sudokuClass.test.js
+++ test/sudokuClass.test.js
@@ -23,4 +23,29 @@ describe("Sudoku", () => {
         assert.ok(result === false);
       });
   });
+
+  describe(".solution", () => {
+    it("should return a 9x9 array", () => {
+        //setup
+        let sudoku = new Sudoku;
+        const solution = sudoku.solution;
+        //exercise
+        const width = solution.length;
+        const height = solution[0].length;
+        //verify
+        assert.ok(width===9 && height===9);
+      });
+
+      it("should only contain values from 1-9", () => {
+        //setup
+        let sudoku = new Sudoku;
+        const solution = sudoku.solution;
+        const flattened = solution.flat();
+        const allowableValues = [1,2,3,4,5,6,7,8,9];
+        //exercise
+        const result = flattened.some(value => !allowableValues.includes(value));
+        //verify
+        assert.ok(!result);
+      });
+  })
 });
