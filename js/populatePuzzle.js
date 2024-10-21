3a133242224e3860756f588d30867e084079e2f2
Meghan Donaghy <meghan.donaghy@zopa.com>
2024-10-21 21:31:09 +0100

    Add functionality to generating buttons

--- js/populatePuzzle.js
+++ js/populatePuzzle.js
@@ -1,4 +1,4 @@
-import Sudoku from './sudokuClass.js';
+import Sudoku from "./sudokuClass.js";
 
 /*
 Idea:
@@ -10,63 +10,89 @@ functions to
     - extension: for inputtable squares right=green, wrong=red, reset colours when changed
 - 
 */
-//generate levels of puzzle
-function generateEasy() {
-  const sudoku = new Sudoku();
-  sudoku.generateSolvedSudoku();
-  sudoku.generateStartingGrid(40); 
-  
-}
 
 //populate html table
 function populateSudokuGrid(grid) {
-    const cells = document.querySelectorAll("#puzzleContainer input");
-    let index = 0;
+  const cells = document.querySelectorAll("#puzzleContainer input");
+  let index = 0;
+  let numberInputs = document.getElementById('numberInputs');
 
-    for (let r = 0; r < grid.length; r++) {
-        for (let c = 0; c < grid[r].length; c++) {
-            const value = grid[r][c];
-            const cell = cells[index];
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      const value = grid[r][c];
+      const cell = cells[index];
+     
+      if (value !== " ") {
+        cell.disabled = true;
+        cell.style.color='black';
+      }
 
-            if (value !== " ") {
-                cell.value = value;
-                cell.disabled = true; // Disable input for pre-filled cells
-            }
-            index++;
-        }
+      cell.value = value;
+
+      index++;
     }
+  }
 }
 
+//generate levels of puzzle
+function generateNewPuzzle(diff) {
+  populateSudokuGrid(
+    Array(9)
+      .fill()
+      .map(() => Array(9).fill(" "))
+  );
+
+  const sudoku = new Sudoku();
+  sudoku.generateSolvedSudoku();
+  sudoku.generateStartingGrid(diff);
+
+
+  populateSudokuGrid(sudoku.startingGrid);
+}
+
+//easy
+let easyPuzzleButton = document.getElementById("easyPuzzleButton");
+easyPuzzleButton.addEventListener("click", () => generateNewPuzzle(40));
+
+//med
+let medPuzzleButton = document.getElementById("medPuzzleButton");
+medPuzzleButton.addEventListener("click", () => generateNewPuzzle(45));
+
+//hard
+let hardPuzzleButton = document.getElementById("hardPuzzleButton");
+hardPuzzleButton.addEventListener("click", () => generateNewPuzzle(50));
+
+//expert
+let expertPuzzleButton = document.getElementById("expertPuzzleButton");
+expertPuzzleButton.addEventListener("click", () => generateNewPuzzle(56));
 
 //Testing Area
 function printSudoku(grid) {
-    console.log(" -----------------------------");
-    for (let i = 0; i < grid.length; i++) {
-      if (i !== 0 && i % 3 === 0) {
-        console.log(" -----------------------------");
-      }
-      let string = "";
-      for (let j = 0; j < grid[i].length; j++) {
-        if (j % 3 === 0) {
-          string += "|";
-        }
-        string += " " + grid[i][j] + " ";
-      }
-      console.log(string + "|");
+  console.log(" -----------------------------");
+  for (let i = 0; i < grid.length; i++) {
+    if (i !== 0 && i % 3 === 0) {
+      console.log(" -----------------------------");
     }
-    console.log(" -----------------------------");
+    let string = "";
+    for (let j = 0; j < grid[i].length; j++) {
+      if (j % 3 === 0) {
+        string += "|";
+      }
+      string += " " + grid[i][j] + " ";
+    }
+    console.log(string + "|");
   }
-  
-  const sudoku = new Sudoku();
-  sudoku.generateSolvedSudoku();
-  sudoku.generateStartingGrid(40);
-  
- /* console.log(sudoku.solution); 
+  console.log(" -----------------------------");
+}
+
+const sudoku = new Sudoku();
+sudoku.generateSolvedSudoku();
+sudoku.generateStartingGrid(40);
+
+/* console.log(sudoku.solution); 
   console.log(sudoku.startingGrid);
   
   console.log(printSudoku(sudoku.solution)); 
   console.log(printSudoku(sudoku.startingGrid)); */
 
-  console.log('hello world');
-  
-  
+console.log("hello world");
